d757b775360cfc913120667ddb52030318cf5625
Deployment Bot (from Travis CI) <deploy@travis-ci.org>
2021-12-12 06:50:15 +0000

    Deploy itsjafer/itsjafer.github.io to github.com/itsjafer/itsjafer.github.io.git:master

---
+++ precache-manifest.392f6a3b8c2e8efc5d2f64a52fe3a627.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b213dc6c4cf6c908041d1bd243489f59",
+    "url": "/index.html"
+  },
+  {
+    "revision": "cf36b344ea0e3015fc35",
+    "url": "/static/css/2.12212f92.chunk.css"
+  },
+  {
+    "revision": "19966971bf5645823528",
+    "url": "/static/css/main.6ac2bb83.chunk.css"
+  },
+  {
+    "revision": "cf36b344ea0e3015fc35",
+    "url": "/static/js/2.54d83db5.chunk.js"
+  },
+  {
+    "revision": "b00d50ddefe818268d972e2eccc55549",
+    "url": "/static/js/2.54d83db5.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "19966971bf5645823528",
+    "url": "/static/js/main.6685bea1.chunk.js"
+  },
+  {
+    "revision": "447edeee62eb2871bcd0",
+    "url": "/static/js/runtime-main.c9a36f42.js"
+  }
+]);
